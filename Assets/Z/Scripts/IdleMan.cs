e7aaabbf19bd07f4618676b955800724e4e50d01
Zilong <>
2019-03-22 16:08:58 +0800

    增加停車格跟斑馬線

---
+++ Assets/Z/Scripts/IdleMan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+public class IdleMan : MonoBehaviour {
+    public GameObject blood;
+
+    public float walkSpeed = 0.1f;
+    public float RANDOM_X_MAX = 13;
+    public float RANDOM_Y_MAX = 20;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+}
